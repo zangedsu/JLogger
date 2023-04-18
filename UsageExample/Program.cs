d3d5c08419ef1dcb099d2201bff28348a5f09998
Vitaliy <zangedsu@gmail.com>
2023-04-18 15:33:01 +0300

    Добавил примеры

--- UsageExample/Program.cs
+++ UsageExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using JLogger;
 
+//создаём экземпляр логгера без параметров
 Logger logger = new Logger();
 
 logger.AddNewRecord("TestRecord");
@@ -10,7 +11,31 @@ try
 }
 catch(Exception ex) { logger.AddNewErrorRecord(ex.Message); }
 
-Console.WriteLine(logger.GetErrorLog());
-Console.WriteLine(logger.GetLog());
+//получаем все записи из журналов, выводим в консоль
+Console.WriteLine("\n" + logger.GetErrorLog());
+Console.WriteLine("\n" + logger.GetLog());
+
 logger.ClearErrorLogs();
 logger.ClearLogs();
+
+//ещё пример использования
+//создаём новый экзмемпляр, но уже с заданными именами файлов
+Logger newLogger = new Logger(@"SecondLog.json", @"SecondErrorLog.json");
+
+newLogger.AddNewRecord("SecondTestRecord1");
+newLogger.AddNewRecord("SecondTestRecord2");
+newLogger.AddNewRecord("SecondTestRecord3");
+
+newLogger.AddNewErrorRecord("SecondTestProblem1");
+newLogger.AddNewErrorRecord("SecondTestProblem2");
+newLogger.AddNewErrorRecord("SecondTestProblem3");
+
+//получаем заданное количество последних записей
+Console.WriteLine("\n" + newLogger.GetErrorLog(2));
+Console.WriteLine("\n" + newLogger.GetLog(2));
+
+newLogger.ClearErrorLogs();
+newLogger.ClearLogs();
+
+
+
